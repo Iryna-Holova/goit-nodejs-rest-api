f475399880136d4d9c34eaa016ee3e582eae9874
Iryna Holova <gis.golova@gmail.com>
2023-06-26 14:07:09 +0300

    add mongoDB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,19 +2,32 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const validateBody = require("../../middlewares/validateBody");
-const schema = require("../../schemas/contacts");
+const isValidBody = require("../../middlewares/isValidBody");
+const isValidId = require("../../middlewares/isValidId");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post("/", validateBody(schema), ctrl.add);
+router.post("/", isValidBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.deleteById);
+router.delete("/:contactId", isValidId, ctrl.deleteById);
 
-router.put("/:contactId", validateBody(schema), ctrl.updateById);
+router.put(
+  "/:contactId",
+  isValidId,
+  isValidBody(schemas.addSchema, "missing fields"),
+  ctrl.updateById
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  isValidBody(schemas.updateFavoriteSchema, "missing field favorite"),
+  ctrl.updateFavorite
+);
 
 module.exports = router;
